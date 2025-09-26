7b7db877dca556524166067b4f97015a0b5c1d83
Heyner Cuevas <73818312+Heyner128@users.noreply.github.com>
2025-09-26 21:54:55 +0200

    Refactor codebase: remove unused methods, replace AntPathRequestMatcher with PathPatternRequestMatcher, streamline SKU deletions, simplify logout API, and eliminate redundant EntityManager usage.

--- src/main/java/me/heyner/stashless/filter/JwtAuthenticationFilter.java
+++ src/main/java/me/heyner/stashless/filter/JwtAuthenticationFilter.java
@@ -6,7 +6,6 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 import java.util.stream.Stream;
 
 import me.heyner.stashless.model.Cookies;
